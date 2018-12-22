1aca45ef83befd722182bd65d8d282d9076c47c6
bak0903 <bak0903@mail.ru>
2018-12-22 17:18:34 +0600

    Создал DeleteView для еды

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
 from webapp.models import Food, OrderFood, Order, Employee
 from webapp.form import FoodForm
 from django.urls import reverse_lazy
 
+
+class FoodListView(ListView):
+    model = Food
+    template_name = 'food_list.html'
 
 class FoodDetailView(DetailView):
     model = Food
@@ -19,16 +23,16 @@ class FoodCreateView(CreateView):
     model = Food
     template_name = 'food_create.html'
     form_class = FoodForm
-    success_url = reverse_lazy('food_detail')
+    success_url = reverse_lazy('food_list')
 
 class FoodUpdateView(UpdateView):
     model = Food
     template_name = 'food_update.html'
     form_class = FoodForm
-    success_url = reverse_lazy('food_detail')
+    success_url = reverse_lazy('food_list')
 
 
 class FoodDeleteView(DeleteView):
     model = Food
     template_name = 'food_delete.html'
-    success_url = reverse_lazy('article_list')
+    success_url = reverse_lazy('food_list')
