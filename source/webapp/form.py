ebba919dc4f969283e2c44fbce7ca7fd2bdf5e77
bak0903 <bak0903@mail.ru>
2018-12-22 18:18:21 +0600

    Стилизовал формы для создания и редактирования еды, убрал форму в отдельный html файл

--- source/webapp/form.py
+++ source/webapp/form.py
@@ -5,4 +5,10 @@ from webapp.models import Food, Order
 class FoodForm(forms.ModelForm):
     class Meta:
         model = Food
-        fields = ['name', 'description', 'photo', 'price']
+        fields = ('name', 'description', 'photo', 'price')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control col-sm-10'}),
+            'description': forms.Textarea(attrs={'class': 'form-control col-sm-10'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control col-sm-10' }),
+        }
+
